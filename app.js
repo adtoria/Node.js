4cde769bdc829e13975621066182f7cb6ba785e4
adtoria <adityyyyadav@gmail.com>
2023-10-21 13:01:32 +0530

    view engines

--- app.js
+++ app.js
@@ -3,26 +3,46 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000); // automatically infers that we need to use localhost
 
 app.get('/', (req, res) => {
     // res.send('<p>home page</p>'); // it infers the type of content automatically & also the status code
-    res.sendFile('./views/index.html', { root: __dirname});
+    // res.sendFile('./views/index.html', { root: __dirname});
+    
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat Bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    
+    // we need to render a view
+    res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
     // res.send('<p>about page</p>'); // it infers the type of content automatically & also the status code
-    res.sendFile('./views/about.html', { root: __dirname});
+    // res.sendFile('./views/about.html', { root: __dirname});
+
+    res.render('about', { title: 'About' });
 });
 
 // redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about');
+// });
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create' });
 });
 
 // 404 page
 // this should be at the very bottom
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname});
+    // res.status(404).sendFile('./views/404.html', { root: __dirname});
+
+    res.status(404).render('404', { title: '404' });
 });
