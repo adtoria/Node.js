474b15ae4bcb6460d33d66b9833734b2b61abbbf
adtoria <adityyyyadav@gmail.com>
2023-10-21 15:54:25 +0530

    refactored

--- app.js
+++ app.js
@@ -20,48 +20,20 @@ mongoose.connect(dbURI)
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
-// app.listen(3000); // automatically infers that we need to use localhost
-
 // middleware & static files (css, images etc)
 app.use(express.static('public'));
-
-// creating middleware:
-// app.use((req, res, next) => {
-//     console.log('new request made:');
-//     console.log('host:', req.hostname);
-//     console.log('path:', req.path);
-//     console.log('method:', req.method);
-//     next();
-// });
-
-// app.use((req, res, next) => {
-//     console.log('in the next middleware');
-//     next();
-// });
 
 // use existing middleware:
 app.use(morgan('dev'));
 
 // routes
-app.get('/', (req, res) => {
-    // res.send('<p>home page</p>'); // it infers the type of content automatically & also the status code
-    // res.sendFile('./views/index.html', { root: __dirname});
-    
+app.get('/', (req, res) => {    
     res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
-    // res.send('<p>about page</p>'); // it infers the type of content automatically & also the status code
-    // res.sendFile('./views/about.html', { root: __dirname});
-
     res.render('about', { title: 'About' });
 });
-
-// redirects
-// app.get('/about-us', (req, res) => {
-//     res.redirect('/about');
-// });
 
 // blog routes
 app.get('/blogs', (req, res) => {
@@ -81,7 +53,5 @@ app.get('/blogs/create', (req, res) => {
 // 404 page
 // this should be at the very bottom
 app.use((req, res) => {
-    // res.status(404).sendFile('./views/404.html', { root: __dirname});
-
     res.status(404).render('404', { title: '404' });
 });
